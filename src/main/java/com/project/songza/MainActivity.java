9d17fb55bc876acec7d0a0c66d47da97cc396901
Cassandra Shum <cassiend@gmail.com>
2013-05-26 19:09:19 -0400

    adding first test

--- src/main/java/com/project/songza/MainActivity.java
+++ src/main/java/com/project/songza/MainActivity.java
@@ -3,10 +3,7 @@ package com.project.songza;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ src/test/java/com.project.songza/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.project.songza;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 5/26/13
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivityTest {
+}
